f07b3767e315e81efa1a2faceb2e98fb5a6160d5
Elaine Candido da Silva <nannicandido@gmail.com>
2024-01-26 12:57:21 -0400

    Added files

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    //This class is used to define the properties and behaviours of a Car
+    //in this software program
+
+    //Property (or class, or member) variables
+    private String color;
+    String make;
+    String model;
+    int numberOfWheels = 4; //The 4 is set as a default. It can be overwritten, but will be used if no other value is provided.
+    boolean isAutomatic;
+    int currentSpeed = 0;           //km/h
+    String driverName;
+
+    //Behaviours or actions a car can do
+    //Constructor method (The special one used for building or INSTANTIATING an object)
+    //Constructor runs when the NEW keyword is used
+//    public Car(String color, String make, String model) {
+//        this.color = color;
+//        this.make = make;
+//        this.model = model;
+//    }
+
+
+    public Car(String color, String make, String model, int numberOfWheels, boolean isAutomatic) {
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.numberOfWheels = numberOfWheels;
+        this.isAutomatic = isAutomatic;
+    }
+
+    public void forward() {}
+    public void reverse() {}
+    public void stop() {
+        //Write the code here to make the car stop
+    }
+    public void accelerate(int rateOfAcceleration) {
+        //Write the code here to make the car accelerate
+        if (rateOfAcceleration < 500) {
+            System.out.println("The starting speed of the " + this.make + " is " + this.currentSpeed + " km/h.");
+            this.currentSpeed += rateOfAcceleration;
+            System.out.println("The new speed, after accelerating, is " + this.currentSpeed + " km/h.");
+        }
+    }
+    public void turnLeft() {}
+    public void turnRight() {}
+    public void beepTheHorn() {
+        System.out.println("The " + this.make + " honks its horn. BEEEEEEEPPPPPPPPP!");
+    }
+    public void doADonut() {
+        System.out.println("The " + this.make + " won the race and does a donut! WOOOOOOOOOHOOOOOOOOOOOOO!");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //psv main is the entry point function that java expects to find and run first
+
+        //Make an instance of a car object
+        Car geoffsCar = new Car("Purple", "Ferrari", "F40", 4, false);
+        Car drewsCar = new Car("Black", "Nissan", "300ZX", 4, false);
+        Car dextersCar = new Car("White", "Mitsubishi", "Minicab", 4, false);
+
+        //Originally, we set the property values manually. Later, we changed it to
+        //send arguments to the constructor method
+//        geoffsCar.color = "Purple";
+//        geoffsCar.make = "Ferrari";
+//        geoffsCar.model = "F40";
+//
+//        drewsCar.color = "Red";
+//        drewsCar.make = "Nissan";
+//        drewsCar.model = "300ZX";
+
+        Car[] theGarage = new Car[3];   //Declare and size the object array
+        theGarage[0] = geoffsCar;
+        theGarage[1] = dextersCar;
+        theGarage[2] = drewsCar;
+
+//        for (int i = 0; i < theGarage.length; i++)
+//        {
+//            theGarage[i].beepTheHorn();
+//            theGarage[i].accelerate(10);
+//        }
+
+        for (Car currentCar : theGarage) {
+            currentCar.beepTheHorn();
+            currentCar.accelerate(15);
+        }
+
+        //Print a message using a value from the geoff car object
+//        System.out.println("Geoff (who is def cool) drives a " + geoffsCar.make + " " + geoffsCar.model);
+        //Call a method of the geoff car object
+//        geoffsCar.beepTheHorn();
+        geoffsCar.accelerate(120);
+        geoffsCar.doADonut();
     }
 }
